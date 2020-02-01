b8961e7f0810bde9af484434b2ead4ccbe0fc6cb
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2020-02-01 14:44:04 -0700

    finished testing[D[D[D[D[D[D[-[F

--- knexfile.js
+++ knexfile.js
@@ -1,32 +1,37 @@
-require('dotenv').config();
-const dbConnection = process.env.DATABASE_URL
+require("dotenv").config();
+const dbConnection = process.env.DATABASE_URL;
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/users.db3' },
+    client: "sqlite3",
+    connection: { filename: "./database/users.db3" },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: "./database/seeds" },
     pool: {
       afterCreate: (conn, done) => {
         conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
+    testing: {
+      client: "sqlite3",
+      connection: {
+        filename: "./database/test.db3"
       },
-  },
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/test.db3',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations',
-    },
-    seeds: {
-      directory: './database/seeds',
-    },
-  },
+      useNullAsDefault: true,
+      migrations: {
+        directory: "./database/migrations"
+      },
+      seeds: {
+        directory: "./database/seeds"
+      },
+      pool: {
+        afterCreate: (conn, done) => {
+          conn.run("PRAGMA foreign_keys = ON", done);
+        }
+      }
+    }
   }
 };
