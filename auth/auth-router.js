c387f85158152b07943b5aa91a8a2682fffc9ed7
mwoodiwiss <51218240+mwoodiwiss@users.noreply.github.com>
2020-01-31 18:41:17 -0700

    Update auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -51,7 +51,7 @@ router.post("/register", async (req, res, next) => {
   
   async function add(user) {
     
-      user.password = await bcrypt.hash(user.password, 10)
+      user.password = await bcrypt.hash(user.password)
     
       const [id] = await db("users")
         .insert(user)
