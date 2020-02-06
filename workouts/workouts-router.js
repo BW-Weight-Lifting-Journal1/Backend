9c67113ce10439630a1c6d39834bd338c636be5c
mwoodiwiss <51218240+mwoodiwiss@users.noreply.github.com>
2020-02-05 20:43:06 -0700

    Update workouts-router.js

--- workouts/workouts-router.js
+++ workouts/workouts-router.js
@@ -46,7 +46,7 @@ router.get("/", (req, res) => {
 router.get(`/:id`, (req, res) => {
   const id = req.params.id;
   db("exercises")
-    .where({ id })
+    .where({ user_id = id })
     .then(exercises => {
       res.json(exercises);
     })
